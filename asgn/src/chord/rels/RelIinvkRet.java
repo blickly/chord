8c8c7c721a325bef484ff723615983072e910aa1
Ben Lickly <blickly@eecs>
2009-11-11 14:55:04 -0800

    IinvkRet

--- asgn/src/chord/rels/RelIinvkRet.java
+++ asgn/src/chord/rels/RelIinvkRet.java
@@ -1,7 +1,13 @@
 package chord.rels;
 
+import chord.program.Var;
+import chord.program.insts.InvkInst;
 import chord.project.Chord;
 import chord.project.ProgramRel;
+
+import chord.doms.DomI;
+import chord.doms.DomZ;
+import chord.doms.DomV;
 
 /**
  * Relation containing each tuple (i,z,v) such that local variable v
@@ -13,6 +19,18 @@ import chord.project.ProgramRel;
 )
 public class RelIinvkRet extends ProgramRel {
 	public void fill() {
-        throw new RuntimeException("cs265: implement this method");
-	}
+            DomI domI = (DomI) doms[0];
+            DomZ domZ = (DomZ) doms[1];
+            DomV domV = (DomV) doms[2];
+            int numI = domI.size();
+            for (int iIdx = 0; iIdx < numI; iIdx++) {
+                InvkInst iVal = domI.get(iIdx);
+                int zIdx = 0;
+                for (Var ret : iVal.getRets()) {
+                        int vIdx = domV.get(ret);
+                        add(iIdx, zIdx, vIdx);
+                        zIdx++;
+                }
+            }
+        }
 }
