478eb45034a07dcb34069f7ab3d20ba36973e0bb
Ben Lickly <blickly@eecs>
2009-11-11 16:31:29 -0800

    MputStatFldInst

--- asgn/src/chord/rels/RelMputStatFldInst.java
+++ asgn/src/chord/rels/RelMputStatFldInst.java
@@ -3,6 +3,13 @@ package chord.rels;
 import chord.project.Chord;
 import chord.project.ProgramRel;
 
+import chord.program.Var;
+import chord.program.CFG;
+import chord.program.Method;
+import chord.program.insts.Inst;
+import chord.program.insts.StatFldRefInst;
+import chord.doms.DomM;
+
 /**
  * Relation containing each tuple (m,f,v) such that method m contains
  * a statement of the form <tt>f = v</tt>.
@@ -12,7 +19,24 @@ import chord.project.ProgramRel;
 	sign = "M0,F0,V0:F0_M0_V0"
 )
 public class RelMputStatFldInst extends ProgramRel {
-	public void fill() {
-        throw new RuntimeException("cs265: implement this method");
-	}
+    public void fill() {
+        DomM domM = (DomM) doms[0];
+        //DomV domV = (DomV) doms[1];
+        //DomF domF = (DomF) doms[2];
+        for (Method meth : domM) {
+            CFG cfg = meth.getCFG();
+            if (cfg == null)
+                continue;
+            for (Inst inst : cfg.getNodes()) {
+                if (inst instanceof StatFldRefInst) {
+                    StatFldRefInst asgn = (StatFldRefInst) inst;
+                    if (asgn.isWr()) {
+                        Var v = asgn.getVar();
+                        if (v == null) continue;
+                        add(meth, v, asgn.getField());
+                    }
+                }
+            }
+        }
+    }
 }
