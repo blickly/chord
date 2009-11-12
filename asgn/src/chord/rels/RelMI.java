68cb2a000f6e3b3de147fc46d2aaa3484a52c6a5
Ben Lickly <blickly@eecs>
2009-11-11 16:37:57 -0800

    MI

--- asgn/src/chord/rels/RelMI.java
+++ asgn/src/chord/rels/RelMI.java
@@ -1,7 +1,14 @@
 package chord.rels;
 
+import chord.program.CFG;
+import chord.program.Method;
+import chord.program.Var;
+import chord.program.insts.Inst;
+import chord.program.insts.InvkInst;
 import chord.project.Chord;
 import chord.project.ProgramRel;
+
+import chord.doms.DomM;
 
 /**
  * Relation containing each tuple (m,i) such that method m contains
@@ -12,7 +19,17 @@ import chord.project.ProgramRel;
 	sign = "M0,I0:I0xM0"
 )
 public class RelMI extends ProgramRel {
-	public void fill() {
-        throw new RuntimeException("cs265: implement this method");
-	}
+    public void fill() {
+        DomM domM = (DomM) doms[0];
+        for (Method meth : domM) {
+            CFG cfg = meth.getCFG();
+            if (cfg == null)
+                continue;
+            for (Inst inst : cfg.getNodes()) {
+                if (inst instanceof InvkInst) {
+                    add(meth, (InvkInst) inst);
+                }
+            }
+        }
+    }
 }
